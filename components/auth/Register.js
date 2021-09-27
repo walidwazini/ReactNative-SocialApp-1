e49523d66afa8e97b56a04d2ca15400e3e9b3b70
Walid Wazini <wazini25@gmail.com>
2021-09-27 13:51:59 +0800

    INput and function

---
+++ components/auth/Register.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { View, Button, TextInput } from 'react-native'
+
+export class Register extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            email: '',
+            password: '',
+            name: 'git '
+        }
+        this.onSignUp = this.onSignUp.bind(this)
+    }
+
+    onSignUp(){
+
+    }
+
+    render() {
+        return (
+            <View>
+                <TextInput
+                    placeholder='name'
+                    onChangeText={(nameParameter) => this.setState({ name : nameParameter })} 
+                    />
+                <TextInput
+                    placeholder='email'
+                    onChangeText={(emailParameter) => this.setState({ emailParameter })} 
+                    />
+                <TextInput
+                    placeholder='password' secureTextEntry='true'
+                    onChangeText={(passwordParameter) => this.setState({ passwordParameter })} 
+                    />
+                <Button onPress={() => this.onSignUp()} title='Sign Up' />
+            </View>
+        )
+    }
+}
+
+export default Register
+
+
+///  USE rce snippet
