6763a7aa27a6de7ac309eedcdd58b00ae5d04f28
Walid Wazini <wazini25@gmail.com>
2021-09-27 14:26:09 +0800

    firebase import and config

--- App.js
+++ App.js
@@ -3,8 +3,18 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as firebase from 'firebase'
 
 import LandingScreen from './components/auth/Landing'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2C-v-IHFbdE9bSKK0qlBGp_VrnoZJtqI",
+  authDomain: "reactnative-socialapp-1.firebaseapp.com",
+  projectId: "reactnative-socialapp-1",
+  storageBucket: "reactnative-socialapp-1.appspot.com",
+  messagingSenderId: "728294444362",
+  appId: "1:728294444362:web:3c6be9d28c43f607e7c5b3"
+};
 
 const Stack = createStackNavigator()
 
